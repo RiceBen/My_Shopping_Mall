4a0714565fa1c6424fd3d240a970376480745a74
benjaminrice <benjamin750715@gmail.com>
2017-12-13 20:11:06 +0800

    add two test case
    car item will not increase if product id is same

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,28 @@
+#購物車類別
 class Car
+  attr_reader :items
+  #初始化購物車
+  def initialize
+    @items = []
+  end
+
+  #新增品項
+  def add_item(product_id)
+    found_item = @items.find { |item| item.product_id == product_id }
+    
+        if found_item
+          found_item.increment
+        else
+          @items << CarItem.new(product_id)
+        end
+  end
+
+  def items
+    @items
+  end
+
+  #判斷購物車是否為空
+  def empty?
+    return @items.empty?
+  end
 end
